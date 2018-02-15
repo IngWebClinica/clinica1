bc508dc9c905c4dfdb137162f0fdd83d1af7eaa3
tunime <tunime.1@gmail.com>
2018-02-15 09:28:01 -0500

    añadiendo los templantes

--- clinica/resources/views/usuario.blade.php
+++ clinica/resources/views/usuario.blade.php
@@ -1,112 +1,57 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="css/main.css">
-    <title>login</title>
-  </head>
-  <body>
-    <div class="container-fluid seccion">
-        <!--menu de arriba-->
-        <div class="container-fluid menu--ariba fixed-top">
-            <ul class=" nav justify-content-end">
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Usuario1</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="#">Salir</a>
-                </li>
-            </ul>
-        </div>
-        <div class="row seccion">
-            <div class="col-2 align-items-stretch menu--lateral">
-                    <ul class="nav flex-column menu_lateral">
-                        <li class="nav-item d-flex  align-items-center menu_lateral--item">
-                            <i class="material-icons">home</i>
-                            <a class="nav-link" href="/inicio">Inicio</a>
-                        </li>
-                        <li class="nav-item d-flex  align-items-center menu_lateral--item">
-                            <i class="material-icons">supervisor_account</i>
-                            <a class="nav-link" href="/usuario">Usuarios</a>
-                        </li>
-                        <li class="nav-item d-flex align-items-center menu_lateral--item">
-                            <i class="material-icons">perm_contact_calendar</i>
-                            <a class="nav-link" href="/pacientes">Pacientes</a>
-                        </li>
-                        <li class="nav-item d-flex  align-items-center menu_lateral--item">
-                            <i class="material-icons">face</i>
-                            <a class="nav-link" href="#">Medicos</a>
-                        </li>
-                        <li class="nav-item d-flex  align-items-center menu_lateral--item">
-                            <i class="material-icons">description</i>
-                            <a class="nav-link" href="#">Atenciones</a>
-                        </li>
-                        <li class="nav-item d-flex  align-items-center menu_lateral--item">
-                            <i class="material-icons">find_in_page</i>
-                            <a class="nav-link" href="#">Reportes</a>
-                        </li>
-                    </ul>
-            </div>
-            <!-- el contenedor de los contenidos -->
-            <div class="col-10 contenedor align-items-stretch d-flex justify-content-center align-items-center">
-                <seccion class="container contenedor--contenido">
-                    <seccion class="row contenedor--contenido_titulo">
-                        <div class="col">
-                            <h3>Lista de usuarios</h3>
-                        </div>
-                    </seccion>
-                    <hr>
-                    <seccion class="row">
-                        <div class="col-3">
-                            <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#nuevousuario">+ nuevo usuario</button>
-                        </div>
-                        <div class="col-9 d-flex justify-content-end">
-                            <form class="form-inline">
-                                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                              </form>
-                        </div>
-                    </seccion>
-                    <seccion class="seccion-tabla">
-                        <table class="table table-striped">
-                            <thead>
-                              <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Nick</th>
-                                <th scope="col">tipo</th>
-                                <th scope="col">Accion</th>
-                              </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($usuarios as $usuario)
-                                    <tr>
-                                            <th scope="row">{{ $usuario->id_usuario }}</th>
-                                            <td>{{ $usuario->nombre }}</td>
-                                            <td>{{ $usuario->nick }}</td>
-                                            <td>{{ $usuario->tipo_usuario }}</td>
-                                            <td>
-                                            <form name="delete" method="DELETE" action="/usuario">
-                                                {{ csrf_field() }}
-                                                <button type="button" class="btn btn-outline-warning">Editar</button>
-                                                <button type="submit" class="btn btn-outline-danger" name="id" value="{{ $usuario->id_usuario }}">Eliminar</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </seccion>
-                    
-                </seccion>
-            </div>
-        </div>
+@extends('template/menu')
+@section('body')
+    <!-- el contenedor de los contenidos -->
+    <div class="col-10 contenedor align-items-stretch d-flex justify-content-center align-items-center">
+        <seccion class="container contenedor--contenido">
+            <seccion class="row contenedor--contenido_titulo">
+                <div class="col">
+                    <h3>Lista de usuarios</h3>
+                </div>
+            </seccion>
+            <hr>
+            <seccion class="row">
+                <div class="col-3">
+                    <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#nuevousuario">+ nuevo usuario</button>
+                </div>
+                <div class="col-9 d-flex justify-content-end">
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                </div>
+            </seccion>
+            <seccion class="seccion-tabla">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Nick</th>
+                        <th scope="col">tipo</th>
+                        <th scope="col">Accion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($usuarios as $usuario)
+                            <tr>
+                                    <th scope="row">{{ $usuario->id_usuario }}</th>
+                                    <td>{{ $usuario->nombre }}</td>
+                                    <td>{{ $usuario->nick }}</td>
+                                    <td>{{ $usuario->tipo_usuario }}</td>
+                                    <td>
+                                    <form name="delete" method="DELETE" action="/usuario">
+                                        {{ csrf_field() }}
+                                        <button type="button" class="btn btn-outline-warning">Editar</button>
+                                        <button type="submit" class="btn btn-outline-danger" name="id" value="{{ $usuario->id_usuario }}">Eliminar</button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </seccion>
+            
+        </seccion>
     </div>
     <!--modales-->
     <div id="nuevousuario" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalPopoversLabel" style="display: none;" aria-hidden="true">
@@ -154,12 +99,5 @@
             </div>
           </div>
         </div>
-      </div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="js/main.js"></script>
-  </body>
-</html>
+    </div>
+@endsection
